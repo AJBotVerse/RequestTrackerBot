26328a868c43d3736906b870656e1e6cff2333c6
AJTimePyro <ajtimepyro@gmail.com>
2021-11-20 19:19:30 +0530

    completed

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@
 
 """Importing"""
 # Importing External Packages
-
 from pyrogram import (
     Client,
     filters
@@ -11,6 +10,7 @@ from pyrogram import (
 from pyrogram.types import (
     Update,
     Message,
+    CallbackQuery,
     InlineKeyboardButton,
     InlineKeyboardMarkup
 )
@@ -83,7 +83,7 @@ async def chatHandler(bot:Update, msg:Message):
         )
     return
 
-@Client.on_message(filters.forwarded & filters.private)
+@app.on_message(filters.forwarded & filters.private)
 async def forwardedHandler(bot:Update, msg:Message):
     if msg.id == Config.OWNER_ID:
         forwardInfo = msg.forward_from_chat
@@ -130,6 +130,7 @@ async def requestHandler(bot:Update, msg:Message):
         return
     else:
         chatInfo = msg.chat
+        
         groupID = environ["GROUPID"]
         channelID = environ["CHANNELID"]
 
@@ -191,5 +192,72 @@ async def requestHandler(bot:Update, msg:Message):
                 )
             )
 
+@app.on_callback_query()
+async def callBackButton(bot:Update, callback_query:CallbackQuery):
+    if callback_query.from_user.id == Config.OWNER_ID:
+        data = callback_query.data
+
+        if data == "reject":
+            result = "REJECTED"
+            groupResult = "has been Rejected."
+            button = InlineKeyboardButton("Request Rejected🚫", "rejected")
+        elif data == "done":
+            result = "COMPLETED"
+            groupResult = "is Completed."
+            button = InlineKeyboardButton("Request Completed✅", "completed")
+        elif data == "unavailable":
+            result = "UNAVAILABLE"
+            groupResult = "has been rejected due to Unavailablity."
+            button = InlineKeyboardButton("Request Rejected🚫", "rejected")
+
+        elif data == "rejected":
+            return await callback_query.answer(
+                "This request Is Completed...\nCheckout in Channel",
+                show_alert = True
+            )
+        elif data == "completed":
+            return await callback_query.answer(
+                "This request is rejected...\nAsk admins in group for more info",
+                show_alert = True
+            )
+
+        msg = callback_query.message
+        originalMsg = msg.text
+        animeRequested = originalMsg.split('#request ')[1]
+        requestedBy = originalMsg.removeprefix("Request by ").split('\n\n')[0]
+        userid = msg.entities[1].user.id
+        mentionUser = f"<a href='tg://user?id={userid}'>{requestedBy}</a>"
+        originalMsgMod = originalMsg.replace(requestedBy, mentionUser)
+        originalMsgMod = f"<s>{originalMsgMod}</s>"
+
+        newMsg = f"<b>{result}</b>\n\n{originalMsgMod}"
+
+        await callback_query.edit_message_text(
+            newMsg,
+            parse_mode = "html",
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        button
+                    ]
+                ]
+            )
+        )
+
+        replyText = f"Dear {mentionUser}\nYour request for {animeRequested} {groupResult}\nThanks for requesting!"
+        groupID = environ["GROUPID"]
+        await bot.send_message(
+            groupID,
+            replyText,
+            parse_mode = "html"
+        )
+    else:
+        await callback_query.answer(
+            "Your are not Owner.",
+            show_alert = True
+        )
+    return
+
 
 app.run()
+
