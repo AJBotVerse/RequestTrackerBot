e5933d5cec6dc4d1277b880549b637f1e474d408
AJTimePyro <ajtimepyro@gmail.com>
2021-11-21 23:16:02 +0530

    removed owner id from config & app.json

--- config.py
+++ config.py
@@ -9,6 +9,5 @@ class Config(object):
     API_ID = int(environ.get("API_ID", 0))
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "")
-    OWNER_ID = int(environ.get("OWNER_ID", 0))
     MONGO_STR = environ.get("MONGO_STR", "")
 
