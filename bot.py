d2b5676734021151680f269c154dcee04f7d8437
AJTimePyro <ajtimepyro@gmail.com>
2021-11-24 12:03:41 +0530

    nothing

--- bot.py
+++ bot.py
@@ -32,10 +32,6 @@ except ModuleNotFoundError:
 # Importing built-in module
 from re import match, search
 
-# Importing Developer defined Module
-from plugin import *
-
-
 app = Client(
     session_name = "RequestTrackerBot",
     api_id = Config.API_ID,
@@ -48,16 +44,9 @@ app = Client(
 mongo_client = MongoClient(Config.MONGO_STR)
 db_bot = mongo_client['RequestTrackerBot']
 collection_ID = db_bot['channelGroupID']
-query = {
-    "groupID" : [
-        "channelID",
-        "userID"
-    ]
-}
-if not collection_ID.find_one(query):
-    collection_ID.insert_one(query)
 
 
+# Regular Expression for #request
 requestRegex = "#[rR][eE][qQ][uU][eE][sS][tT] "
 
 
@@ -258,7 +247,7 @@ async def requestHandler(bot:Update, msg:Message):
         try:
             document[groupID]
         except KeyError:
-            pass
+            continue
         else:
             channelID = document[groupID][0]
             fromUser = msg.from_user
@@ -320,81 +309,88 @@ async def requestHandler(bot:Update, msg:Message):
                     ]
                 )
             )
-        finally:
-            return
+            break
+    return
+        
 
 @app.on_callback_query()
 async def callBackButton(bot:Update, callback_query:CallbackQuery):
     channelID = str(callback_query.message.chat.id)
-    document = collection_ID.find_one(query)
 
-    resultID = idExtractor(channelID, document)
-    if resultID:
-        groupID, channelOwnerID = resultID
+    documents = collection_ID.find()
+    for document in documents:
+        for key in document:
+            if key == "_id":
+                continue
+            else:
+                if document[key][0] != channelID:
+                    continue
+                else:
+                    groupID = key
 
-        data = callback_query.data
-        if data == "rejected":
-            return await callback_query.answer(
-                "This request is rejected💔...\nAsk admins in group for more info💔",
-                show_alert = True
-            )
-        elif data == "completed":
-            return await callback_query.answer(
-                "This request Is Completed🥳...\nCheckout in Channel😊",
-                show_alert = True
-            )
-        if callback_query.from_user.id == channelOwnerID:
+                    data = callback_query.data
+                    if data == "rejected":
+                        return await callback_query.answer(
+                            "This request is rejected💔...\nAsk admins in group for more info💔",
+                            show_alert = True
+                        )
+                    elif data == "completed":
+                        return await callback_query.answer(
+                            "This request Is Completed🥳...\nCheckout in Channel😊",
+                            show_alert = True
+                        )
+                    user = await bot.get_chat_member(int(channelID), callback_query.from_user.id)
+                    if user.status not in ("administrator", "creator"):
+                        await callback_query.answer(
+                            "Who the hell are you?\nYour are not Owner😒.",
+                            show_alert = True
+                        )
+                    else:
+                        if data == "reject":
+                            result = "REJECTED"
+                            groupResult = "has been Rejected💔."
+                            button = InlineKeyboardButton("Request Rejected🚫", "rejected")
+                        elif data == "done":
+                            result = "COMPLETED"
+                            groupResult = "is Completed🥳."
+                            button = InlineKeyboardButton("Request Completed✅", "completed")
+                        elif data == "unavailable":
+                            result = "UNAVAILABLE"
+                            groupResult = "has been rejected💔 due to Unavailablity🥲."
+                            button = InlineKeyboardButton("Request Rejected🚫", "rejected")
 
-            if data == "reject":
-                result = "REJECTED"
-                groupResult = "has been Rejected💔."
-                button = InlineKeyboardButton("Request Rejected🚫", "rejected")
-            elif data == "done":
-                result = "COMPLETED"
-                groupResult = "is Completed🥳."
-                button = InlineKeyboardButton("Request Completed✅", "completed")
-            elif data == "unavailable":
-                result = "UNAVAILABLE"
-                groupResult = "has been rejected💔 due to Unavailablity🥲."
-                button = InlineKeyboardButton("Request Rejected🚫", "rejected")
+                        msg = callback_query.message
+                        originalMsg = msg.text
+                        findRegexStr = search(requestRegex, originalMsg)
+                        requestString = findRegexStr.group()
+                        contentRequested = originalMsg.split(requestString)[1]
+                        requestedBy = originalMsg.removeprefix("Request by ").split('\n\n')[0]
+                        userid = msg.entities[1].user.id
+                        mentionUser = f"<a href='tg://user?id={userid}'>{requestedBy}</a>"
+                        originalMsgMod = originalMsg.replace(requestedBy, mentionUser)
+                        originalMsgMod = f"<s>{originalMsgMod}</s>"
 
-            msg = callback_query.message
-            originalMsg = msg.text
-            findRegexStr = search(requestRegex, originalMsg)
-            requestString = findRegexStr.group()
-            contentRequested = originalMsg.split(requestString)[1]
-            requestedBy = originalMsg.removeprefix("Request by ").split('\n\n')[0]
-            userid = msg.entities[1].user.id
-            mentionUser = f"<a href='tg://user?id={userid}'>{requestedBy}</a>"
-            originalMsgMod = originalMsg.replace(requestedBy, mentionUser)
-            originalMsgMod = f"<s>{originalMsgMod}</s>"
+                        newMsg = f"<b>{result}</b>\n\n{originalMsgMod}"
 
-            newMsg = f"<b>{result}</b>\n\n{originalMsgMod}"
+                        await callback_query.edit_message_text(
+                            newMsg,
+                            parse_mode = "html",
+                            reply_markup = InlineKeyboardMarkup(
+                                [
+                                    [
+                                        button
+                                    ]
+                                ]
+                            )
+                        )
 
-            await callback_query.edit_message_text(
-                newMsg,
-                parse_mode = "html",
-                reply_markup = InlineKeyboardMarkup(
-                    [
-                        [
-                            button
-                        ]
-                    ]
-                )
-            )
-
-            replyText = f"<b>Dear {mentionUser}🧑\nYour request for {contentRequested} {groupResult}\n👍Thanks for requesting!</b>"
-            await bot.send_message(
-                int(groupID),
-                replyText,
-                parse_mode = "html"
-            )
-        
-        else:
-            await callback_query.answer(
-                "Who the hell are you?\nYour are not Owner😒.",
-                show_alert = True
-            )
+                        replyText = f"<b>Dear {mentionUser}🧑\nYour request for {contentRequested} {groupResult}\n👍Thanks for requesting!</b>"
+                        await bot.send_message(
+                            int(groupID),
+                            replyText,
+                            parse_mode = "html"
+                        )
+                    return
     return
 
 
